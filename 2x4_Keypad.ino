f7103223c10e8facb632a6a4c8bc073c3b0ee624
mltMatt <matt@mlttek.com>
2025-09-14 12:35:11 -0600

    Marcos and modes working, split defines

--- 2x4_Keypad.ino
+++ 2x4_Keypad.ino
@@ -1,108 +1,59 @@
+#include "defines.h"
+
 #include <Adafruit_NeoPixel.h>
+#include <Keyboard.h>
 
-// Pins
-#define BUTTON_A_PIN   5
-#define BUTTON_B_PIN   6
-#define BUTTON_RESET   2
-#define LED_PIN        3
-
-// LEDs
-#define NUM_LEDS       2
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// Button debounce
-unsigned long lastDebounceTimeA = 0;
-unsigned long lastDebounceTimeB = 0;
-unsigned long lastDebounceTimeR = 0;
-const unsigned long debounceDelay = 50;
-
-int lastButtonStateA = HIGH;
-int lastButtonStateB = HIGH;
-int lastButtonStateR = HIGH;
-
-// Colors (now using strip.Color)
-uint32_t blueMid;
-uint32_t greenBright;
-uint32_t greenMid;
-uint32_t grey;
-uint32_t lightGrey;
-uint32_t mintLight;
-uint32_t red;
-uint32_t green;
-uint32_t blue;
-uint32_t lightPurple;
-uint32_t yellow;
-uint32_t darkYellow;
-uint32_t teal;
-uint32_t orange;
-uint32_t dullRed;
-uint32_t FOLDER;
-uint32_t FOX;
-uint32_t EDIT;
-uint32_t CALC;
-uint32_t colorOff;
-
-uint32_t defaultColorA;
-uint32_t defaultColorB;
-
-uint32_t pressedColorA;
-uint32_t pressedColorB;
-
-uint8_t brightness = 176; // 0-255
-
-uint8_t buttonA = 0;
-uint8_t buttonB = 1;
-uint8_t buttonR = 2;
-
+// ---------------- SETUP ----------------
 void setup() {
   pinMode(BUTTON_A_PIN, INPUT_PULLUP);
   pinMode(BUTTON_B_PIN, INPUT_PULLUP);
   pinMode(BUTTON_RESET, INPUT_PULLUP);
 
   strip.begin();
-  strip.setBrightness(brightness);
+  strip.setBrightness(176);
 
   // Define colors
-  blueMid      = strip.Color(0, 17, 65);
-  greenBright  = strip.Color(0, 128, 0);
-  greenMid     = strip.Color(0, 42, 5);
-  grey         = strip.Color(5, 5, 5);
-  lightGrey    = strip.Color(12, 20, 20);
-  mintLight    = strip.Color(28, 74, 44);
-  red          = strip.Color(75, 5, 5);
-  green        = strip.Color(5, 75, 5);
-  blue         = strip.Color(5, 5, 75);
-  lightPurple  = strip.Color(35, 35, 95);
-  yellow       = strip.Color(45, 45, 5);
-  darkYellow   = strip.Color(15, 15, 5);
-  teal         = strip.Color(5, 35, 25);
-  orange       = strip.Color(100, 50, 0);
-  dullRed      = strip.Color(26, 0, 0);
-  FOLDER       = strip.Color(95, 77, 28);
-  FOX          = strip.Color(200, 54, 8);
-  EDIT         = strip.Color(50, 115, 25);
-  CALC         = strip.Color(53, 56, 59);
-  colorOff     = strip.Color(0, 0, 0);
+  blueMid       = strip.Color(0, 17, 65);
+  greenBright   = strip.Color(0, 128, 0);
+  greenMid      = strip.Color(0, 42, 5);
+  grey          = strip.Color(5, 5, 5);
+  lightGrey     = strip.Color(12, 20, 20);
+  mintLight     = strip.Color(28, 74, 44);
+  red           = strip.Color(75, 5, 5);
+  green         = strip.Color(5, 75, 5);
+  blue          = strip.Color(5, 5, 75);
+  lightPurple   = strip.Color(35, 35, 95);
+  yellow        = strip.Color(45, 45, 5);
+  darkYellow    = strip.Color(15, 15, 5);
+  teal          = strip.Color(5, 35, 25);
+  orange        = strip.Color(100, 50, 0);
+  dullRed       = strip.Color(26, 0, 0);
+  FOLDER        = strip.Color(95, 77, 28);
+  FOX           = strip.Color(200, 54, 8);
+  EDIT          = strip.Color(50, 115, 25);
+  CALC          = strip.Color(53, 56, 59);
+  off           = strip.Color(0, 0, 0);
 
-  // Assign defaults
-  defaultColorA = blueMid;
-  defaultColorB = green;
+  // Now that colors exist, configure modes
+  modes[0] = { blueMid, red, green, orange };   // Mode 0: Copy / Paste
+  modes[1] = { yellow, teal, lightPurple, red };     // Mode 1: Find / Save
 
-  pressedColorA = red;
-  pressedColorB = orange;
+  // Set initial mode colors
+  applyModeColors();
 
-  // Initial state
-  strip.setPixelColor(buttonA, defaultColorA);
-  strip.setPixelColor(buttonB, defaultColorB);
-  strip.show();
+  Keyboard.begin();
 }
 
+// ---------------- LOOP ----------------
 void loop() {
   handleButton(BUTTON_A_PIN, lastButtonStateA, lastDebounceTimeA, buttonA);
   handleButton(BUTTON_B_PIN, lastButtonStateB, lastDebounceTimeB, buttonB);
   handleButton(BUTTON_RESET, lastButtonStateR, lastDebounceTimeR, buttonR);
 }
 
+// ---------------- BUTTON HANDLER ----------------
 void handleButton(int pin, int &lastState, unsigned long &lastTime, int buttonID) {
   int reading = digitalRead(pin);
 
@@ -114,26 +65,66 @@ void handleButton(int pin, int &lastState, unsigned long &lastTime, int buttonID
     if (reading == LOW) {
       // Button pressed
       if (buttonID == buttonA) {
-        strip.setPixelColor(buttonA, pressedColorA);
-      } else if (buttonID == buttonB) {
-        strip.setPixelColor(buttonB, pressedColorB);
-      } else if (buttonID == buttonR) {
-        strip.setPixelColor(buttonA, colorOff);
-        strip.setPixelColor(buttonB, colorOff);
+        strip.setPixelColor(buttonA, modes[currentMode].colorA_pressed);
+        if (millis() - lastKeyTimeA > keyRepeatDelay) {
+          performAction(buttonA);
+          lastKeyTimeA = millis();
+        }
+      } 
+      else if (buttonID == buttonB) {
+        strip.setPixelColor(buttonB, modes[currentMode].colorB_pressed);
+        if (millis() - lastKeyTimeB > keyRepeatDelay) {
+          performAction(buttonB);
+          lastKeyTimeB = millis();
+        }
+      } 
+      else if (buttonID == buttonR) {
+        // Cycle mode
+        currentMode = (currentMode + 1) % totalModes;
+        applyModeColors();
       }
       strip.show();
     } else {
-      // Button released
+      // Button released → restore defaults
       if (buttonID == buttonA) {
-        strip.setPixelColor(buttonA, defaultColorA);
+        strip.setPixelColor(buttonA, modes[currentMode].colorA_default);
       } else if (buttonID == buttonB) {
-        strip.setPixelColor(buttonB, defaultColorB);
-      } else if (buttonID == buttonR) {
-        strip.setPixelColor(buttonA, defaultColorA);
-        strip.setPixelColor(buttonB, defaultColorB);
+        strip.setPixelColor(buttonB, modes[currentMode].colorB_default);
       }
       strip.show();
     }
   }
   lastState = reading;
+}
+
+// ---------------- ACTIONS ----------------
+void performAction(int buttonID) {
+  switch (currentMode) {
+    case 0: // Copy / Paste
+      if (buttonID == buttonA) {
+        Keyboard.press(KEY_LEFT_CTRL); Keyboard.press('c');
+        delay(10); Keyboard.releaseAll();
+      } else if (buttonID == buttonB) {
+        Keyboard.press(KEY_LEFT_CTRL); Keyboard.press('v');
+        delay(10); Keyboard.releaseAll();
+      }
+      break;
+
+    case 1: // Find / Save
+      if (buttonID == buttonA) {
+        Keyboard.press(KEY_LEFT_CTRL); Keyboard.press('f');
+        delay(10); Keyboard.releaseAll();
+      } else if (buttonID == buttonB) {
+        Keyboard.press(KEY_LEFT_CTRL); Keyboard.press('s');
+        delay(10); Keyboard.releaseAll();
+      }
+      break;
+  }
+}
+
+// ---------------- HELPERS ----------------
+void applyModeColors() {
+  strip.setPixelColor(buttonA, modes[currentMode].colorA_default);
+  strip.setPixelColor(buttonB, modes[currentMode].colorB_default);
+  strip.show();
 }
---
+++ defines.h
@@ -0,0 +1,64 @@
+// Pins
+#define BUTTON_A_PIN   5
+#define BUTTON_B_PIN   6
+#define BUTTON_RESET   2
+#define LED_PIN        3
+
+// LEDs
+#define NUM_LEDS       2
+
+// Button debounce
+unsigned long lastDebounceTimeA = 0;
+unsigned long lastDebounceTimeB = 0;
+unsigned long lastDebounceTimeR = 0;
+const unsigned long debounceDelay = 50;
+
+// Key repeat suppression
+unsigned long lastKeyTimeA = 0;
+unsigned long lastKeyTimeB = 0;
+const unsigned long keyRepeatDelay = 500; // ms, tweakable
+
+int lastButtonStateA = HIGH;
+int lastButtonStateB = HIGH;
+int lastButtonStateR = HIGH;
+
+// Button IDs
+uint8_t buttonA = 0;
+uint8_t buttonB = 1;
+uint8_t buttonR = 2;
+
+// ---------------- COLORS ----------------
+uint32_t blueMid;
+uint32_t greenBright;
+uint32_t greenMid;
+uint32_t grey;
+uint32_t lightGrey;
+uint32_t mintLight;
+uint32_t red;
+uint32_t green;
+uint32_t blue;
+uint32_t lightPurple;
+uint32_t yellow;
+uint32_t darkYellow;
+uint32_t teal;
+uint32_t orange;
+uint32_t dullRed;
+uint32_t FOLDER;
+uint32_t FOX;
+uint32_t EDIT;
+uint32_t CALC;
+uint32_t off;
+
+// ---------------- MODE CONFIG ----------------
+struct ModeConfig {
+  uint32_t colorA_default;
+  uint32_t colorA_pressed;
+  uint32_t colorB_default;
+  uint32_t colorB_pressed;
+};
+
+// Allocate space - filled in setup()
+ModeConfig modes[2];
+
+int currentMode = 0;
+const int totalModes = sizeof(modes) / sizeof(modes[0]);
