3c222f5def128bb34c22c061204d1f7cf6c8155b
mltMatt <matt@mlttek.com>
2025-09-14 20:51:38 -0600

    Comment update

--- defines.h
+++ defines.h
@@ -16,7 +16,7 @@ const unsigned long debounceDelay = 10;
 // Key repeat suppression
 unsigned long lastKeyTimeA = 0;
 unsigned long lastKeyTimeB = 0;
-const unsigned long keyRepeatDelay = 0; // ms, tweakable
+const unsigned long keyRepeatDelay = 0;
 
 int lastButtonStateA = HIGH;
 int lastButtonStateB = HIGH;
